11562287b88ae7c40a3cc7b1fdb23601cba8bc9b
Kuma <Kuma@Macs-MacBook-Pro.local>
2017-04-30 15:13:55 -0500

    finished the calculator, including the addition of the brand new clear button

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,115 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    var buttonSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValue = ""
+    var rightValue = ""
+    var result = ""
+    var currentOperation = Operation.Empty
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "empty "
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundUrl =  URL(fileURLWithPath: path!)
+        
+        do {
+            try buttonSound = AVAudioPlayer(contentsOf: soundUrl)
+            buttonSound.prepareToPlay()
+        } catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
+        outputLabel.text = "0"
+    }
+    
+    @IBAction func pressedDivide(sender: UIButton) {
+        processOperation(operation: .Divide)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pressedMultiply(sender: UIButton) {
+        processOperation(operation: .Multiply)
     }
-
-
+    
+    @IBAction func pressedSubtract(sender: UIButton) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func pressedAdd(sender: UIButton) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func pressedEquals(sender: UIButton) {
+        processOperation(operation: currentOperation)
+    }
+    
+    @IBAction func pressedClear(sender: UIButton) {
+        playSound()
+        outputLabel.text = "0"
+        runningNumber = ""
+        leftValue = ""
+        rightValue = ""
+        result = ""
+        currentOperation = .Empty
+    }
+    
+    @IBAction func pressedNumber(sender: UIButton) {
+        playSound()
+        runningNumber += "\(sender.tag)"
+        outputLabel.text = runningNumber
+    }
+    
+    func playSound() {
+        if buttonSound.isPlaying {
+            buttonSound.stop()
+        }
+        buttonSound.play()
+    }
+    
+    func processOperation(operation: Operation) {
+        playSound()
+        if currentOperation != Operation.Empty {
+            if runningNumber != ""  {
+                rightValue = runningNumber
+                runningNumber = ""
+                
+                switch currentOperation {
+                case Operation.Multiply:
+                    result = "\(Double(leftValue)! * Double(rightValue)!)"
+                case Operation.Divide:
+                    result = "\(Double(leftValue)! / Double(rightValue)!)"
+                case Operation.Subtract:
+                    result = "\(Double(leftValue)! - Double(rightValue)!)"
+                case Operation.Add:
+                    result = "\(Double(leftValue)! + Double(rightValue)!)"
+                default:
+                    result = ""
+                }
+                
+                leftValue = result;
+                outputLabel.text = result
+            }
+            currentOperation = operation
+        } else {
+            leftValue = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
+    
 }
-
